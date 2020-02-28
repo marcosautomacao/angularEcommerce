c138cdf6d458edc1dbd2e84edcbe42060cb339b5
marcosautomacao <marcosmoura00@hotmail.com>
2020-02-27 23:08:34 -0300

    servicos de detalhes de produto

--- src/app/products.ts
+++ src/app/products.ts
@@ -11,14 +11,7 @@ export const products = [
   },
   {
     name: 'Phone Standard',
-    price: 299,
+    price: 1299,
     description: ''
   }
 ];
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
